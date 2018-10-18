8ddd92681b3832e2e1a71e38dca803a3addf423e
Qian Zhou <qzhou@stuy.edu>
2018-10-18 13:21:35 -0400

    a

--- dbUpdate.py
+++ dbUpdate.py
@@ -9,8 +9,10 @@ def addStories(usern, storyName, contrib):
     cmd = "INSERT INTO {} VALUES(?,?,?)".format(storyName)
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-    vals= [(usern), (st), (contrib)]
+    vals= [[(usern), (st), (contrib)]]
     c.executemany(cmd, vals)
+    db.commit()
+    db.close()
     return
 
 addStories("qzhou", "Frankenstein","who made him?")
