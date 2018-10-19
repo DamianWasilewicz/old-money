73560e63d02fba3ead50a3ef829ebf56f3da2b02
Qian Zhou <qzhou@stuy.edu>
2018-10-19 13:18:18 -0400

    a

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def check():
             print(sha256_crypt.hash(passw))
             if sha256_crypt.verify(passw, entry[1]):
                 
-                return 'SUCCESS!!\n<a href="/display?story=Frankenstein">view stories</a>'
+                return '''SUCCESS!!\n<a href="/display?story=Frankenstein">view stories</a>\n<a href="/editPage?story=Frankenstein">edit stories</a>'''
             return "NAY PASSWORD"
     return "no such username"
 
@@ -45,6 +45,19 @@ def display():
     for txt in contributions:
         s += txt[0]+" "
     return s
+
+@app.route("/editPage")#edit stories
+def editPage():
+    DB_FILE = "data/stories.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor() #facilitate db ops
+    nm = request.args.get("story")
+    # don't know how to select last addition
+    cmd = """SELECT * FROM """+nm
+    lastC =""
+    contributions = c.execute(cmd).fetchall()
+    lastC = contributions[len(contributions)-1][2]
+    return render_template("story.html", title = nm, content = lastC)
 
 if __name__ == "__main__":
     app.debug = True
